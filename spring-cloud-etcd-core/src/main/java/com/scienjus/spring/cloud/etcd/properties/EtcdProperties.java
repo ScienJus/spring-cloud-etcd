537adccea0ca85e4419365a31f6b442e529875bc
xieenlong <xieenlong@mobike.com>
2018-05-31 15:30:30 +0800

    wrapped singletonList

--- spring-cloud-etcd-core/src/main/java/com/scienjus/spring/cloud/etcd/properties/EtcdProperties.java
+++ spring-cloud-etcd-core/src/main/java/com/scienjus/spring/cloud/etcd/properties/EtcdProperties.java
@@ -3,8 +3,10 @@ package com.scienjus.spring.cloud.etcd.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 /**
  * properties for etcd
@@ -15,5 +17,5 @@ import java.util.List;
 @ConfigurationProperties("spring.cloud.etcd")
 public class EtcdProperties {
 
-  private List<String> endpoints = Arrays.asList("http://127.0.0.1:2379");
+  private List<String> endpoints = new ArrayList<>(singletonList("http://127.0.0.1:2379"));
 }
