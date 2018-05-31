7fb51374ae117a2be7318fa7a2d3c991b8b6fa86
xieenlong <xieenlong@mobike.com>
2018-05-31 15:15:03 +0800

    use Arrays#asList instead of Collections#singletonList

--- spring-cloud-etcd-core/src/main/java/com/scienjus/spring/cloud/etcd/properties/EtcdProperties.java
+++ spring-cloud-etcd-core/src/main/java/com/scienjus/spring/cloud/etcd/properties/EtcdProperties.java
@@ -3,7 +3,7 @@ package com.scienjus.spring.cloud.etcd.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,5 +15,5 @@ import java.util.List;
 @ConfigurationProperties("spring.cloud.etcd")
 public class EtcdProperties {
 
-  private List<String> endpoints = Collections.singletonList("http://127.0.0.1:2379");
+  private List<String> endpoints = Arrays.asList("http://127.0.0.1:2379");
 }
