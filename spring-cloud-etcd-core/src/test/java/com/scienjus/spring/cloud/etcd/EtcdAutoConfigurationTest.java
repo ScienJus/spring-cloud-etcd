c4890690a26b88e40aba5be1e4e61c73877a0b76
xieenlong <xieenlong@mobike.com>
2018-06-03 19:43:35 +0800

    single file for testing EtcdProperties

--- spring-cloud-etcd-core/src/test/java/com/scienjus/spring/cloud/etcd/EtcdAutoConfigurationTest.java
+++ spring-cloud-etcd-core/src/test/java/com/scienjus/spring/cloud/etcd/EtcdAutoConfigurationTest.java
@@ -5,14 +5,10 @@ import com.coreos.jetcd.KV;
 import com.coreos.jetcd.data.ByteSequence;
 import com.coreos.jetcd.data.KeyValue;
 import com.coreos.jetcd.kv.GetResponse;
-import com.scienjus.spring.cloud.etcd.properties.EtcdProperties;
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
@@ -21,36 +17,6 @@ import static org.springframework.boot.test.util.EnvironmentTestUtils.addEnviron
 public class EtcdAutoConfigurationTest {
 
     private final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
-    @Test
-    public void testEtcdPropertiesWithNoneEndpoint() {
-        this.context.register(EtcdAutoConfiguration.class);
-        this.context.refresh();
-        EtcdProperties etcdProperties = this.context.getBean(EtcdProperties.class);
-        assertEquals(etcdProperties.getEndpoints(), Collections.singletonList("http://127.0.0.1:2379"));
-    }
-
-    @Test
-    public void testEtcdPropertiesWithSingleEndpoint() {
-        String endpoint = "http://10.0.0.1:2379";
-        addEnvironment(this.context, "spring.cloud.etcd.endpoints[0]=" + endpoint);
-        this.context.register(EtcdAutoConfiguration.class);
-        this.context.refresh();
-        EtcdProperties etcdProperties = this.context.getBean(EtcdProperties.class);
-        assertEquals(etcdProperties.getEndpoints(), Collections.singletonList(endpoint));
-    }
-
-    @Test
-    public void testEtcdPropertiesWithMultipleEndpoints() {
-        List<String> endpoints = Arrays.asList("http://10.0.0.1:2379", "http://10.0.0.2:2379");
-        for (int i = 0; i < endpoints.size(); i++) {
-            addEnvironment(this.context, String.format("spring.cloud.etcd.endpoints[%d]=%s", i, endpoints.get(i)));
-        }
-        this.context.register(EtcdAutoConfiguration.class);
-        this.context.refresh();
-        EtcdProperties etcdProperties = this.context.getBean(EtcdProperties.class);
-        assertEquals(etcdProperties.getEndpoints(), endpoints);
-    }
 
     @Test
     public void testAfterConfigurationLoadedEtcdClientIsAvailable() throws ExecutionException, InterruptedException {
